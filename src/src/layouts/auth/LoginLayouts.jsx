6edb4a5445e94058ab0316bcc3318bcd8e76c9bf
NandaCoba <anandalukman517@gmail.com>
2025-07-23 07:28:53 +0700

    add license and readme

--- src/src/layouts/auth/LoginLayouts.jsx
+++ src/src/layouts/auth/LoginLayouts.jsx
@@ -6,7 +6,7 @@ const LoginLayouts = () => {
     <div className="w-96 h-96 rounded-xl border border-zinc-600 ">
       <div className="bg-zinc-700 opacity-25 absolute inset-0 rounded-xl z-0"></div>
         <div className="relative z-10 justify-center items-center grid space-y-5 ">
-        <h1>Login</h1>
+        <h1 className=' '>Login</h1>
         <div className=' grid space-y-5'>
           <InputAuth name={"Username"} />
           <InputAuth name={"Password"} />
